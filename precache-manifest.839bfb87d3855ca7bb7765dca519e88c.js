3ddb055dd5f141869bc26997a664ff8f82671c2a
codingFer <shencirujano@gmail.com>
2019-12-30 09:18:05 -0400

    Updates

---
+++ precache-manifest.839bfb87d3855ca7bb7765dca519e88c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/learnReactWithPlatzi/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "32e1880b2be4c1bbfdc9",
+    "url": "/learnReactWithPlatzi/static/css/main.c5522a10.chunk.css"
+  },
+  {
+    "revision": "c029a35ae21e44b57608",
+    "url": "/learnReactWithPlatzi/static/js/runtime~main.c029a35a.js"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/learnReactWithPlatzi/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "74447293aa92b2704078",
+    "url": "/learnReactWithPlatzi/static/js/2.74447293.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/learnReactWithPlatzi/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "32e1880b2be4c1bbfdc9",
+    "url": "/learnReactWithPlatzi/static/js/main.32e1880b.chunk.js"
+  },
+  {
+    "revision": "d9e93b6469c794a453ff2b1c603a5f89",
+    "url": "/learnReactWithPlatzi/static/media/twitter.d9e93b64.jpg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/learnReactWithPlatzi/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/learnReactWithPlatzi/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "74447293aa92b2704078",
+    "url": "/learnReactWithPlatzi/static/css/2.5d084d6a.chunk.css"
+  },
+  {
+    "revision": "9fb7bdf97b3073f7491dfa26b90190dc",
+    "url": "/learnReactWithPlatzi/index.html"
+  }
+];
